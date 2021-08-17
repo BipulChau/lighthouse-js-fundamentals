6d21749386e4765dd4e2f43360137d22da5f9443
Bipul Chaudhary <bipulchau@gmail.com>
2021-08-17 14:38:12 -0400

    Add more conditionl examples

--- conditionals.js
+++ conditionals.js
@@ -18,7 +18,7 @@ if (cold){
   console.log("Short sleeves are fine.");
 }
 
-const temperature=-2;
+let temperature=-43;
 if (temperature<0){
   console.log("Make sure you pick out a scarf.");
 } else if (temperature<15){
@@ -28,5 +28,25 @@ if (temperature<0){
 }
 
 console.log("Now you are ready to go outside.");
+
+const isCitizen=true;
+const age=35;
+
+if (isCitizen && age>18){
+  console.log("You are eligible to vote.");
+}
+
+
+if (temperature<-40 || temperature>40){
+  console.log("May be going outside is not such a great idea.");
+}
+
+if (!raining){
+  console.log("Leave your umbrella at home.");
+}
+
+
+
+
 
 
