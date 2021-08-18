2caf81864f7f85c7dd95dff0e1f8de506c15c3d3
Bipul Chaudhary <bipulchau@gmail.com>
2021-08-18 15:45:50 -0400

    check even number'

---
+++ even.js
@@ -0,0 +1,12 @@
+const isEven = function (num) {
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven (10);
+const eleveIsEven = isEven(11);
+
+console.log (tenIsEven);
+console.log (eleveIsEven);
+ console.log (isEven(27));
+ console.log (isEven(14));
+ 
